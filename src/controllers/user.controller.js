3e2df2f895e3be881f4bb8194c7088901fe8efa3
rainydayinjune <55171736+rainydayinjune@users.noreply.github.com>
2021-03-04 00:18:03 +0700

    change createUser to register usercontroller
    change createUser to register usercontroller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,7 +6,7 @@ const userService = require('../services/user.service');
 * @param {import('express').Response} res
 * @param {import('express').NextFunction} next
 */
-module.exports.createUser = async (req, res, next) => {
+module.exports.register = async (req, res, next) => {
   console.log(JSON.stringify(req.body));
   res.json({
     data: req.body
--- src/routers/routers.js
+++ src/routers/routers.js
@@ -9,12 +9,12 @@ const { checkAuthenticate } = require('../middlewares/auth');
 module.exports.setupRouters = (app) => {
     //Auth
     app.post('/login', authController.login);
-  
+
     //Verify Midleware
     app.all('/api/*', checkAuthenticate);
 
     // Api for user
-    app.post('/api/v1/users', userController.createUser);
+    app.post('/register', userController.register);
     // major service 
     app.get('/getMajor', majorController.getMajorDetail);
 
