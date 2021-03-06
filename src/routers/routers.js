9a6e2b49a1425bfabce8e593b437aac86269ad7a
anhndse05746 <anhnd128@gmail.com>
2021-03-07 01:32:53 +0700

    update routes.js

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -13,16 +13,15 @@ module.exports.setupRouters = (app) => {
 
     //Verify Midleware
     app.all('/api/*', checkAuthenticate);
-
-    //Verify Role Middleware
+    //Verify Admin Middleware
     app.all('/api/admin/*', verifyRole.checkRole);
 
     //API for register
     app.post('/register', userController.register);
 
+    //Admin APIs
     //API for get all customers
     app.get('/api/admin/getAllCus', userController.getAllCustomerController);
-
     //API for get all repairers
     app.get('/api/admin/getAllRepairer', repairerController.getAllRepairerController);
 };
