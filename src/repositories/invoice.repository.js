323a7e79388e297ac6e8cffde38c852818c55460
namnd <namnd@vnext.com.vn>
2021-04-10 16:33:30 +0700

    xác nhận thanh toán

--- src/repositories/invoice.repository.js
+++ src/repositories/invoice.repository.js
@@ -1,4 +1,5 @@
 const Invoice = require('../models/invoice');
+const constants = require('../utils/constants');
 
 
 // lay ra data cua major (service, issues)
@@ -14,3 +15,14 @@ module.exports.createInvoice = async (request_id, payment_method_id, status, cos
     return invoice;
 }
 
+module.exports.confirmInvoice = async (request_id) => {
+    return await Invoice.update({
+        status: constants.STATUS_INVOICE_DONE
+    }, {
+        where: {
+            request_id: request_id
+        }
+    }).then().catch(err => {
+        throw new Error(err.message);
+    });
+}
