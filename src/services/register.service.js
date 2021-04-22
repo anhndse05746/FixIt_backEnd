0b72f95276e675993f184fd9b50d7c45725b7c30
rainydayinjune <meola.9x@gmail.com>
2021-04-22 11:00:54 +0700

    Update bcrypt password for Register, Reset Password, ChangePassword Check duplicated old and newPassword when reset password.

--- src/services/register.service.js
+++ src/services/register.service.js
@@ -2,20 +2,21 @@ const user = require('../models/user');
 const userRegister = require('../models/user');
 const constants = require('../utils/constants');
 const Repairer = require('../models/repairer');
+const bcrypt = require('bcrypt');
 
-module.exports.register = async (phone_number, password, name, role_id, email, identity_card,
-    major_id, district, city) => {
-    
+module.exports.register = async (phone_number, password, name, role_id, email, identity_card, major_id, district, city) => {
+
+    const hashedPassword = await bcrypt.hash(password, 10);
     let registerCheck = await user.findOne({
         where: {
             phone_number: phone_number,
             role_id: role_id
         }
-    }).then( async (user) => {
-        if(!user) {
+    }).then(async (user) => {
+        if (!user) {
             let resultRegister = await userRegister.create({
                 phone_number: phone_number,
-                password: password,
+                password: hashedPassword,
                 name: name,
                 role_id: role_id,
                 email: email,
@@ -23,7 +24,7 @@ module.exports.register = async (phone_number, password, name, role_id, email, i
             }).then().catch(err => {
                 throw new Error(err.message);
             });
-            if(role_id == 2) {
+            if (role_id == 2) {
                 let registeredUser = await userRegister.findOne({
                     where: {
                         phone_number: phone_number,
@@ -36,7 +37,7 @@ module.exports.register = async (phone_number, password, name, role_id, email, i
                     id: registeredUser.id,
                     major_id: major_id,
                     identity_card_number: identity_card,
-                    is_verify: 0, 
+                    is_verify: 0,
                     district: district,
                     city: city
                 }).then().catch(err => {
